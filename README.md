74fddcaab5e1ad3ef14e664cf5da09c7884ecb4e
eren23 <erenak1996@gmail.com>
2021-06-22 21:57:06 +0200

    initial commit, image collectors

---
+++ README.md
@@ -0,0 +1,7 @@
+# Utility Scripts for Computer Vision Projects
+
+### Image Collector From Video Feed
+
+Use video recordings or your webcam to harvest images from video feed.
+Inspired from [here](https://github.com/nicknochnack/TFODCourse), removed some parts because I needed faster image creation so I gave up on the automated process.
+
---
+++ image_collector.py
@@ -0,0 +1,44 @@
+from posixpath import join
+import cv2
+import uuid
+import os
+import time
+import subprocess
+
+labels = ["humanface", "humanbody"] # Name for the folder to be created
+
+sample_size = 10 # How much do we need from each picture
+
+IMAGE_PATH = os.path.join("images","collected") #Path
+
+
+currentdir = os.getcwd() # Current dir
+
+joined = os.path.join( currentdir, IMAGE_PATH) # Joined path
+cap = cv2.VideoCapture(0)
+
+# print(joined)
+
+if not os.path.exists(joined):
+    os.makedirs(joined) # If images folder are not exists
+
+for label in labels:
+    # for each label new path and folders
+    new_path = os.path.join(joined, label) 
+    # print(new_path)
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+
+
+for img in range(sample_size):
+    print(f"Image number {img}")
+    success, frame = cap.read()
+    imgname = os.path.join(joined, labels[0], labels[0]+'.'+'{}.jpg'.format(str(uuid.uuid1()))) #select the path and create unique named image names
+    cv2.imwrite(imgname, frame) # write image
+    cv2.imshow("frame", frame) # show the feed
+        # time.sleep(3)
+
+    if cv2.waitKey(1) & 0XFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
