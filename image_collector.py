4aaaa9d61d1610e3b68856dce0fddc21e24eee36
eren23 <erenak1996@gmail.com>
2021-07-03 20:18:21 +0200

    basic camera checker

--- image_collector.py
+++ image_collector.py
@@ -5,7 +5,7 @@ import os
 import time
 import subprocess
 
-labels = ["humanface", "humanbody"] # Name for the folder to be created
+labels = ["thumbsup", "thumbsdown", "thankyou", "livelong"] # Name for the folder to be created
 
 sample_size = 10 # How many images do we need
 
@@ -33,9 +33,10 @@ for label in labels:
 for img in range(sample_size):
     print(f"Image number {img}")
     success, frame = cap.read()
-    imgname = os.path.join(joined, labels[0], labels[0]+'.'+'{}.jpg'.format(str(uuid.uuid1()))) #select the path and create unique named image names
+    imgname = os.path.join(joined, labels[3], labels[3]+'.'+'{}.jpg'.format(str(uuid.uuid1()))) #select the path and create unique named image names
     cv2.imwrite(imgname, frame) # write image
     cv2.imshow("frame", frame) # show the feed
+    time.sleep(2)
         # time.sleep(3)
 
     if cv2.waitKey(1) & 0XFF == ord("q"):
